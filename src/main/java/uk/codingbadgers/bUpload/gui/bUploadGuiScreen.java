cc539055cfc58c0914bd19f7ef2cba13d03f9518
James Fitzpatrick <thefishlive@hotmail.com>
2014-01-08 17:58:14 +0000

    Version bump for minecraft update

--- src/main/java/uk/codingbadgers/bUpload/gui/bUploadGuiScreen.java
+++ src/main/java/uk/codingbadgers/bUpload/gui/bUploadGuiScreen.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.Tessellator;
 
 public class bUploadGuiScreen extends GuiScreen {
 
+	private static final float SCALE = 0.00390625F;
 	protected bUploadGuiScreen parent;
 
 	public bUploadGuiScreen(bUploadGuiScreen screen) {
@@ -38,14 +39,12 @@ public class bUploadGuiScreen extends GuiScreen {
 	}
 
 	public void drawTexturedModalRectSized(int x, int y, int u, int v, int width, int height, int uvwidth, int uvheight) {
-		final float var7 = 0.00390625F;
-		final float var8 = 0.00390625F;
 		Tessellator quad = Tessellator.instance;
 		quad.startDrawingQuads();
-		quad.addVertexWithUV((double) (x), (double) (y + height), (double) zLevel, (double) (u * var7), (double) ((v + uvheight) * var8));
-		quad.addVertexWithUV((double) (x + width), (double) (y + height), (double) zLevel, (double) ((u + uvwidth) * var7), (double) ((v + uvheight) * var8));
-		quad.addVertexWithUV((double) (x + width), (double) (y), (double) zLevel, (double) ((u + uvwidth) * var7), (double) (v * var8));
-		quad.addVertexWithUV((double) (x), (double) (y), (double) zLevel, (double) (u * var7), (double) (v * var8));
+		quad.addVertexWithUV((double) (x), (double) (y + height), (double) zLevel, (double) (u * SCALE), (double) ((v + uvheight) * SCALE));
+		quad.addVertexWithUV((double) (x + width), (double) (y + height), (double) zLevel, (double) ((u + uvwidth) * SCALE), (double) ((v + uvheight) * SCALE));
+		quad.addVertexWithUV((double) (x + width), (double) (y), (double) zLevel, (double) ((u + uvwidth) * SCALE), (double) (v * SCALE));
+		quad.addVertexWithUV((double) (x), (double) (y), (double) zLevel, (double) (u * SCALE), (double) (v * SCALE));
 		quad.draw();
 	}
 
