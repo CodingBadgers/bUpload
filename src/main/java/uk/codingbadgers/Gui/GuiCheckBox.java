c05dccceaf5397e8b5d83a111dec67340cda6fe6
James Fitzpatrick <thefishlive@hotmail.com>
2014-01-08 19:32:13 +0000

    Fix localizations some what, closes #1

--- src/main/java/uk/codingbadgers/Gui/GuiCheckBox.java
+++ src/main/java/uk/codingbadgers/Gui/GuiCheckBox.java
@@ -20,7 +20,6 @@ package uk.codingbadgers.Gui;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.util.ResourceLocation;
